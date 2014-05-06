3dd9cd3fd6af309a561e47a51a2880b381cac4ce
fionhan <1056856566@qq.com>
2014-05-06 21:53:50 +0800

    init project

---
+++ FilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FilterDemo.Filters;
+
+namespace FilterDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[MyAuthorization(Users = "bob,amy,penny", Roles = "admin")]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+        //Add By Hpp On 2014-5-6 测试失败,待查
+        //[AjaxAuthorizationAttribute]
+        //[MyAuthorization(Users = "bob,amy,penny", Roles = "admin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FilterDemo/Filters/MyAuthorizationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilterDemo.Filters
+{
+    public class MyAuthorizationAttribute:AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            //return httpContext.Request.IsLocal || base.AuthorizeCore(httpContext);
+            return false;
+        }
+    }
+}
